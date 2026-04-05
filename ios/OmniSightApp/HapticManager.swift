b056f6f9198da8ead4368678bc395caf4be41bec
Abba Yadav <support@orbconcepts.com>
2026-04-05 12:00:00 -0400

    Implemented HapticManager for alert feedback

---
+++ ios/OmniSightApp/HapticManager.swift
@@ -0,0 +1,38 @@
+// OmniSight - Visual Navigation System
+// Personal Project - Humanized Codebase
+
+
+import UIKit
+
+// This class makes the phone vibrate for alerts!
+class HapticManager {
+    static let shared = HapticManager()
+    
+    private let light = UIImpactFeedbackGenerator(style: .light)
+    private let medium = UIImpactFeedbackGenerator(style: .medium)
+    private let heavy = UIImpactFeedbackGenerator(style: .heavy)
+    private let notification = UINotificationFeedbackGenerator()
+    
+    private init() {
+        light.prepare()
+        medium.prepare()
+        heavy.prepare()
+        notification.prepare()
+    }
+    
+    func smallVibration() {
+        // small vibrate
+        light.impactOccurred()
+    }
+    
+    func mediumVibration() {
+        medium.impactOccurred()
+    }
+    
+    func warningVibration() {
+        // BIG VIBRATE FOR WHEN YOU ARE GOING TO HIT SOMETHING
+        // DON'T IGNORE THIS
+        heavy.impactOccurred()
+        notification.notificationOccurred(.error)
+    }
+}
